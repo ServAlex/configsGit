884b50e13fa92a74184695b9a1084615e680c940
Alex Servirog <servalex1@gmail.com>
2017-07-06 01:41:30 +0300

    added script to create mountpoints for prime

---
+++ createMountPoints.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo mkdir /run/media/alex/media_2TB
+sudo chmod 777 /run/media/alex/media_2TB
+
+sudo mkdir /run/media/alex/data_static
+sudo chmod 777 /run/media/alex/data_static
+
+sudo mkdir /run/media/alex/data_install
+sudo chmod 777 /run/media/alex/data_install
+
+sudo mkdir /run/media/alex/ins_main_EXEC
+sudo chmod 777 /run/media/alex/ins_main_EXEC
+
+sudo mkdir /run/media/alex/data_media
+sudo chmod 777 /run/media/alex/data_media
+
+sudo mkdir /run/media/alex/publicNAS_mount
+sudo chmod 777 /run/media/alex/publicNAS_mount
+
